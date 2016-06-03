1116e2bb4e38150df25267e4bea64403e42eabff
tinkazorge <tinkazorge@hotmail.com>
2016-06-03 13:02:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tinkazorge/pointclickgame/TextPopUps.java
@@ -0,0 +1,28 @@
+package com.example.tinkazorge.pointclickgame;
+
+/**
+ *this method returns the string that needs to pop up when a certain object is clicked.
+ */
+public class TextPopUps {
+    public void flush (){
+
+    }
+    public void toilet(){
+
+    }
+    public void chicken(){
+
+    }
+    public void snake(){
+
+    }
+    public void fatchicken(){
+
+    }
+    public void eggs(){
+
+    }
+    public void feather(){
+
+    }
+}
